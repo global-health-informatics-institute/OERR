5ad2f56a5ca348e7aa5b8c7c3bf0c496fc6e8f14
Thembi01 <sithembiphombeya@gmail.com>
2023-03-21 10:43:55 +0200

    led_control edited

--- utils/led_control.py
+++ utils/led_control.py
@@ -4,10 +4,20 @@ import RPi.GPIO as GPIO
 class led_control:
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(23, GPIO.OUT)
+    GPIO.setup(20, GPIO.OUT)
+    GPIO.setup(27, GPIO.OUT)
 
     def turn_led_off(self):
-        GPIO.output(23, GPIO.LOW)
+        GPIO.output(20, GPIO.LOW)
 
     def turn_led_on(self):
-        GPIO.output(23, GPIO.HIGH)
+        GPIO.output(20, GPIO.HIGH)
+
+    def charger_led_green(self):
+        GPIO.output(27, GPIO.LOW)
+
+    def charger_led_red(self):
+        GPIO.output(27, GPIO.HIGH)
+
+    def charger_led_off(self):
+        GPIO.output(27, GPIO.LOW)

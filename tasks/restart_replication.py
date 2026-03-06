8902c31f5f36d9c174121cf3eae98b096edd3b5c
89Tain <manda.titani@gmail.com>
2026-03-06 11:51:48 +0200

    feat(replication): add a skip window for  archiving enhance: add logging and clear the 'restart_replication.log' file

--- tasks/restart_replication.py
+++ tasks/restart_replication.py
@@ -2,20 +2,14 @@ import json
 import subprocess
 import datetime
 import sys
-
-now = datetime.datetime.now()
-
-start_time = now.replace(hour=7, minute=45, second=0, microsecond=0)
-end_time = now.replace(hour=8, minute=15, second=0, microsecond=0)
-
-if start_time <= now <= end_time:
-    sys.exit(0)
+import logging
 
 
+logging.basicConfig(level=logging.INFO)
 
 replications_file = "config/replications.config"
 ward_file = "config/department.config"
-log_file = "logs/replication_errors.log"
+log_file = "logs/restart_replication.log"
 
 with open(replications_file) as json_file:
     replication_settings = json.load(json_file)
@@ -23,6 +17,17 @@ with open(replications_file) as json_file:
 with open(ward_file) as json_file:
     wards_data = json.load(json_file)
 
+START_HOUR = replication_settings["threshhold"]["start_hour"] | 1
+START_MINUTE = replication_settings["threshhold"]["start_minute"] | 0
+START_SECOND = replication_settings["threshhold"]["start_second"] | 0
+START_MICROSECOND = replication_settings["threshhold"]["start_microsecond"] | 0
+
+END_HOUR = replication_settings["threshhold"]["end_hour"] | 3
+END_MINUTE = replication_settings["threshhold"]["end_minute"] | 0
+END_SECOND = replication_settings["threshhold"]["end_second"]   | 0
+END_MICROSECOND = replication_settings["threshhold"]["end_microsecond"] | 0
+
+# wards list for the specific department to be used in the filter function of the replication
 department_name = replication_settings["specific_department"]["department"]
 wards = []
 for department in wards_data["departments"]:
@@ -30,19 +35,53 @@ for department in wards_data["departments"]:
         wards = department["wards"]
         break
 
+
+now = datetime.datetime.now()
+
+start_time = now.replace(hour=START_HOUR, minute=START_MINUTE, second=START_SECOND, microsecond=START_MICROSECOND)
+end_time = now.replace(hour=END_HOUR, minute=END_MINUTE, second=END_SECOND, microsecond=END_MICROSECOND)
+
+if start_time <= now  <= end_time:
+    with open(log_file, 'a') as log:
+        log.write(f"{datetime.datetime.now()} - Replication skipped at {now} as it is within the threshhold time window.\n")
+        logging.info(f"{now} : as it is within the threshhold time window.")
+    sys.exit(0)
+
+
+
 source_url = f"http://{replication_settings['source']['user']}:{replication_settings['source']['passwd']}@{replication_settings['source']['host']}:{replication_settings['source']['port']}/{replication_settings['source_base_db']['database']}"
 target_url = f"http://{replication_settings['target']['user']}:{replication_settings['target']['password']}@{replication_settings['target']['host']}:{replication_settings['target']['port']}/{replication_settings['target_base_db']['database']}"
 replicator_db_url = f"http://{replication_settings['source']['user']}:{replication_settings['source']['passwd']}@{replication_settings['source']['host']}:{replication_settings['source']['port']}/_replicator"
 
+# make log file empty at the start of each run
+with open(log_file, 'w') as log:
+    log.write(f"{datetime.datetime.now()} - Starting replication restart process...\n")
+    logging.info(f"Starting replication restart process...")
+
+# delete replicator database if it exists
+delete_replicator_db_cmd = ['curl', '-X', 'DELETE', replicator_db_url]
+try:
+    subprocess.run(delete_replicator_db_cmd, check=True, capture_output=True, text=True)
+    logging.info(f"Deleting _replicator database if it exists...")
+    with open(log_file, 'a') as log:
+        log.write(f"{datetime.datetime.now()} - Deleting _replicator database if it exists...\n")
+except subprocess.CalledProcessError as e:
+    with open(log_file, 'a') as log:
+        log.write(f"{datetime.datetime.now()} - Error deleting _replicator database: {e.stderr}\n")
+
+
+# create new replicator database
 create_replicator_db_cmd = ['curl', '-X', 'PUT', replicator_db_url]
 try:
     subprocess.run(create_replicator_db_cmd, check=True, capture_output=True, text=True)
     with open(log_file, 'a') as log:
-        log.write(f"Created replicator databese:\n")
+        log.write(f"{datetime.datetime.now()} - Created replicator databese:\n")
+        logging.info(f"Created replicator databese...")
 except subprocess.CalledProcessError as e:
     with open(log_file, 'a') as log:
-        log.write(f"Error creating _replicator database: {e.stderr}\n")
-
+        log.write(f"{datetime.datetime.now()} - Error creating _replicator database: {e.stderr}\n")
+        logging.error(f"Error creating _replicator database: {e.stderr}")
+# create design document for filtered replication from target to source based on wards of the specific department
 design_id = (replication_settings["source"]["host"]).replace('.','')
 
 design_doc = {
@@ -58,20 +97,24 @@ create_design_doc_cmd = [
 try:
     result = subprocess.run(create_design_doc_cmd, check=True, capture_output=True, text=True)
     with open(log_file, 'a') as log:
-        log.write(f"Design document created successfully on the target database\n")
+        log.write(f"{datetime.datetime.now()} - Design document created successfully on the target database\n")
+        logging.info(f"Design document created successfully on the target database")
 
 except subprocess.CalledProcessError as e:
     with open(log_file, 'a') as log:
-        log.write(f"Error creating design document: {e.stderr}\n")
+        log.write(f"{datetime.datetime.now()} - Error creating design document: {e.stderr}\n")
+        logging.error(f"Error creating design document: {e.stderr}")
 
 def execute_replication(command, log_message):
     try:
         result = subprocess.run(command, check=True, capture_output=True, text=True)
         with open(log_file, 'a') as log:
-            log.write(f"Replication setup:\n")
+            log.write(f"{datetime.datetime.now()} - {log_message}:\n")
+            logging.info(log_message)
     except subprocess.CalledProcessError as e:
         with open(log_file, 'a') as log:
-            log.write(f"Error: {log_message}\n{e.stderr}\n")
+            log.write(f"{datetime.datetime.now()} - Error: {log_message}\n{e.stderr}\n")
+            logging.error(f"Error: {log_message}\n{e.stderr}")
 
 source_to_target_cmd = [
     'curl', '-d', json.dumps({
@@ -127,5 +170,6 @@ for suffix in sub_directories:
         }), '-H', 'Content-Type: application/json', '-X', 'POST', replicator_db_url
     ]
 
-    execute_replication(source_to_target_cmd, f"Replication setup from source to target for {suffix}")
-    execute_replication(target_to_source_cmd_ltp, f"Replication setup from target to source for {suffix}")
+# if suffix is "" use "primary"
+    execute_replication(source_to_target_cmd, f"{datetime.datetime.now()} - Replication setup from source to target for {suffix if suffix else 'primary'}")
+    execute_replication(target_to_source_cmd_ltp, f"{datetime.datetime.now()} - Replication setup from target to source for {suffix if suffix else 'primary'}")

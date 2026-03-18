e28aea8579d4afd3a16c2de6e0c3d4b9b1b4f64a
89Tain <manda.titani@gmail.com>
2026-03-18 14:48:40 +0200

    feat: add automated CouchDB archiving with bulk migration and patient status updates
    refactor: modularize archive script,

--- tasks/archive.py
+++ tasks/archive.py
@@ -5,260 +5,257 @@ from datetime import datetime, timedelta
 import logging
 from tqdm import tqdm
 
+# CONFIG
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-basis_file = "config/basis.config"
+application_file = "config/application.config"
+
+archive_file = "config/archive.config"
+
+with open(application_file) as json_file:
+    application_settings = json.load(json_file)
 
 
-with open(basis_file) as json_file:
-    basis_settings = json.load(json_file)
-url = f"http://{basis_settings['couch']['host']}:{basis_settings['couch']['port']}"
-DB = f"{url}/{basis_settings['couch']['database']}"
-username = f"{basis_settings['couch']['user']}"
-password = f"{basis_settings['couch']['passwd']}"
-database = f"{basis_settings['couch']['database']}"
+with open(archive_file) as json_file:
+    archive_settings = json.load(json_file)
 
 
+# URL, TARGETS, PARAMS
+USERNAME = f"{application_settings['couch']['user']}"
+PASSWORD = f"{application_settings['couch']['passwd']}"
+DATABASE = f"{application_settings['couch']['database']}"
+HOST = f"{application_settings['couch']['host']}"
+PORT = f"{application_settings['couch']['port']}"
+URL = f"http://{HOST}:{PORT}"
+DB_BASE_URL = f"{URL}/{DATABASE}"
 
-DB_BASE = f"{url}/"
-DB = f"{url}/{database}"
 
-# CodeA_Helper
-def ensure_database_exists(database_name):
-    if database_name != f"{database}":
-        database_name = f"{database}_{database_name}"
+JOB_DB = ["", "_active", "_archive"]
 
-    address = f"{DB_BASE}{database_name}"
+PATIENT_URL = f"{DB_BASE_URL}_patients"
+
+
+CUT_OFF_DAYS = int(archive_settings.get("cut_off_days", 8)) 
+cut_off_date = datetime.now() - timedelta(days=CUT_OFF_DAYS)
+CUT_OFF_DATE = int(cut_off_date.timestamp())
+
+DELETE_ARCHIVE = archive_settings.get("delete_archive", False)
+
+# CREATE DB IF NOT EXIST
+def _helper_create_db(database_name):
+    create_request = requests.put(f"{DB_BASE_URL}{database_name}", auth=HTTPBasicAuth(USERNAME, PASSWORD))
+    return create_request.status_code
+
+def _helper_check_db_if_exist(database_name):
+    check_request = requests.head(f"{DB_BASE_URL}{database_name}", auth=HTTPBasicAuth(USERNAME, PASSWORD))
+    return check_request.status_code
+
+def create_db_if_not_exist():
+    try:
+        for db in JOB_DB:
+            if _helper_check_db_if_exist(db) == 404:
+                logging.info(f"Database does not exist: {db}")
+                if _helper_create_db(db) == 201:
+                    logging.info(f"Database created: {db}")
+                else:
+                    logging.error(f"Error creating db: {db}")
+            else:
+                logging.info(f"Database already exists: {db}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error: (check db if exist) - {e}")
+
+# LOAD ENTRIES
+def _helper_request_docs(batch_url):
+    get_docs_request = requests.get(batch_url, auth=HTTPBasicAuth(username=USERNAME, password=PASSWORD))
+    return get_docs_request.status_code, get_docs_request.json() if get_docs_request.status_code == 200 else None
+
+def _helper_unpack_docs(data, batch_size):
+    rows = data.get('rows', [])
+    documents = [row['doc'] for row in rows]
+
+    if len(rows) < batch_size:
+        return documents, None
+
+    return documents, f'"{rows[-1]["id"]}"'  # second value is last key
+
+
+def load_entries(batch_size=9000):
+    batch_url = f"{DB_BASE_URL}/_all_docs?include_docs=true&limit={batch_size}"
+    last_key = None
+    all_docs = []
 
     try:
-        response = requests.get(address, auth=HTTPBasicAuth(username, password))
+        while True:
+            url = batch_url
+            if last_key:
+                url += f"&startkey={last_key}&skip=1"
 
-        if response.status_code == 404:
-            create_db_response = requests.put(address, auth=HTTPBasicAuth(username, password))
+            status_code, data = _helper_request_docs(batch_url=url)
 
-            if create_db_response.status_code == 201:
-                logging.info(f"Database '{database_name}' created successfully.")
-            else:
-                logging.error(f"Failed to create database '{database_name}': {create_db_response.status_code} - {create_db_response.text}")
-        elif response.status_code == 200:
-            logging.info(f"Database '{database_name}' already exists.")
-        else:
-            logging.error(f"Error connecting to the database '{database_name}': {response.status_code} - {response.text}")
-
-    except requests.exceptions.RequestException as e:
-        logging.error(f"An error occurred while connecting to '{database_name}': {str(e)}")
-        return False
-    
-    return True
-
-# CodeA
-def initialize_setup():
-    db_list = [f"{database}", "active"]
-    for db in db_list:
-        ensure_database_exists(db)
-
-# CodeB
-def fetch_entries(batch_size=9000):
-    all_docs_url = f"{DB}/_all_docs?include_docs=true&limit={batch_size}"
-    last_key = None
-    all_documents = []
-
-    while True:
-        url = all_docs_url
-        if last_key:
-            url += f"&startkey={last_key}"
-
-        response = requests.get(url, auth=HTTPBasicAuth(username, password))
-        if response.status_code == 200:
-            data = response.json()
-            rows = data.get('rows', [])
-            documents = [row['doc'] for row in rows]
-            all_documents.extend(documents)
-
-            if len(rows) < batch_size:
+            if status_code != 200 or not data:
                 break
 
-            last_key = f'"{rows[-1]["id"]}"'
+            docs, last_key = _helper_unpack_docs(data, batch_size)
+            all_docs.extend(docs)
 
-        else:
-            f"Error fetching documents: {response.status_code} - {response.text}"
-            break
+            if not last_key:
+                break
 
-    return all_documents
+    except Exception as e:
+        logging.error(f"Error: (loading entries) - {e}")
+        return []
 
-# CodeC
-def filter_entries():
-    documents = fetch_entries()
-    if not documents:
-        logging.warning("No documents found.")
-        return [], []
+    return all_docs
 
-    active_documents = []
-    archive_documents = []
-    
-    eight_days_ago = datetime.now() - timedelta(days=8)
+# FILTER ENTRIES
+"""
+returns an array of active docs, archive docs and archived docs patient owner
+active_docs, archive_docs, patient_id
+"""
 
-    for doc in tqdm(documents, desc="Filtering entries", unit="doc"):
-        date_ordered_timestamp = doc.get('date_ordered')
-        if date_ordered_timestamp:
-            date_ordered = datetime.fromtimestamp(date_ordered_timestamp)
+def _helper_get_patient_id(document):
+    patient_id = document.get('patient_id')
+    return patient_id or None
 
-            if date_ordered > eight_days_ago:
-                active_documents.append(doc)
+
+def filter_entries(all_entries):
+    if not all_entries:
+        logging.warning("Warning: No documents found")
+        return [], [], []
+
+    active_entries = []
+    archive_entries = []
+    patient_ids = []
+
+    for entry in tqdm(all_entries, desc="filtering + sorting"):
+        date_ordered = entry.get('date_ordered')
+        if date_ordered:
+            if date_ordered > CUT_OFF_DATE:
+                active_entries.append(entry)
             else:
-                archive_documents.append(doc)
+                archive_entries.append(entry)
+                patient_id = _helper_get_patient_id(entry)
+                if patient_id:
+                    patient_ids.append(patient_id)
+    if patient_ids:
+        patient_ids = list(set(patient_ids))
 
-    return active_documents, archive_documents
+    return active_entries, archive_entries, patient_ids
 
-# CodeD1
-def update_patient_records(archive_documents):
-    """
-    Update the patient records to mark them as archived.
-    """
-    patient_db_base_url = f"{DB_BASE}{database}_patients/"
 
-    for doc in tqdm(archive_documents, desc="Updating patient status", unit="doc"):
-        patient_id = doc.get('patient_id')
-        if not patient_id:
-            f"No patient ID found for document: {doc.get('_id')}"
-            continue
+# MARK PATIENTS
+def _get_patient_doc(patient_id):
+    response = requests.get(
+        url=f"{PATIENT_URL}/{patient_id}",
+        auth=HTTPBasicAuth(username=USERNAME, password=PASSWORD),
+    )
+    if response.status_code == 200:
+        raw_doc = response.json()
+        raw_doc["archived"] = True
+        return raw_doc
+    return None
 
-        patient_url = f"{patient_db_base_url}{patient_id}"
-        
-        try:
-            response = requests.get(patient_url, auth=HTTPBasicAuth(username, password))
 
-            if response.status_code == 200:
-                patient_doc = response.json()
-                patient_doc["archived"] = True 
-
-                save_response = requests.put(patient_url, json=patient_doc, auth=HTTPBasicAuth(username,password))
-
-                if save_response.status_code in [200, 201]:
-                    f"Patient '{patient_id}' updated successfully."
-                else:
-                    f"Failed to update patient '{patient_id}': {save_response.status_code} - {save_response.text}"
-
-            else:
-                f"Failed to fetch patient '{patient_id}': {response.status_code} - {response.text}"
-
-        except requests.exceptions.RequestException as e:
-            f"Error occurred while updating patient '{patient_id}': {str(e)}"
-
-# CodeD
-def save_active_entries(active_documents):
-    """
-    Save active documents into the active database.
-    """
-    if not active_documents:
-        logging.info("No active documents to save.")
-        return
-
-    active_db = f"{DB_BASE}{database}_active"
-    
-    if not ensure_database_exists("active"):
-        logging.error("Failed to ensure 'oerr_active' database exists.")
-        return
-    
-    for doc in tqdm(active_documents, desc="Saving active entries", unit="doc"):
-        if '_rev' in doc:
-            del doc['_rev']
-        
-        doc_id = doc.get('_id')
-        if not doc_id:
-            f"Document without '_id' found: {doc}"
-            continue
-
-        save_url = f"{active_db}/{doc_id}"
-
-        try:
-            response = requests.put(save_url, json=doc, auth=HTTPBasicAuth(username, password))
-
-            if response.status_code in [200, 201]:
-                f"Document '{doc_id}' saved successfully."
-            else:
-                f"Failed to save document '{doc_id}': {response.status_code} - {response.text}"
-        
-        except requests.exceptions.RequestException as e:
-            f"Error occurred while saving document '{doc_id}': {str(e)}"
-
-# codeE
-def house_keeping_please(db_name):
-    drop_db_url = f"{DB_BASE}{db_name}"
-
-    try:
-        response = requests.delete(drop_db_url, auth=HTTPBasicAuth(username, password))
-
-        if response.status_code == 200:
-            logging.info(f"Database {db_name} deleted successfully.")
-        elif response.status_code == 404:
-            logging.warning(f"Database {db_name} not found.")
-        else:
-            logging.error(f"Failed to delete database {db_name}: {response.status_code} - {response.text}")
-
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Error occurred while deleting the {db_name} database: {str(e)}")
-
-# codeF
-def exodus():
-    # Set up the source and target databases
-    source_db = f"{DB_BASE}{database}_active"
-    target_db = f"{DB_BASE}{database}"
-    batch_size = 9000
-    last_key = None
-
-    while True:
-        # Fetch a batch of documents from oerr_active
-        url = f"{source_db}/_all_docs?include_docs=true&limit={batch_size}"
-        if last_key:
-            url += f"&startkey={last_key}"
-
-        response = requests.get(url, auth=HTTPBasicAuth(username, password))
-        if response.status_code != 200:
-            f"Error fetching documents from '{database}_active': {response.status_code} - {response.text}"
-            break
-
-        data = response.json()
-        rows = data.get('rows', [])
-        if not rows:
-            break
-
-        for row in tqdm(rows, desc="Exodus process", unit="doc"):
-            doc = row.get('doc')
-            if not doc:
-                continue
-
-            if '_rev' in doc:
-                del doc['_rev']
-
-            doc_id = doc.get('_id')
-            if not doc_id:
-                logging.warning(f"Document without '_id' found: {doc}")
-                continue
-
-            save_url = f"{target_db}/{doc_id}"
-
-            try:
-                save_response = requests.put(save_url, json=doc, auth=HTTPBasicAuth(username, password))
-
-                if save_response.status_code not in [200, 201]:
-                    f"Failed to save document '{doc_id}' to '{database}': {save_response.status_code} - {save_response.text}"
+def _patch_request(patient_id):
+    patient_doc = _get_patient_doc(patient_id=patient_id)
+    if patient_doc:
+        patch_patient_request = requests.put(
+            url=f"{PATIENT_URL}/{patient_id}",
+            auth=HTTPBasicAuth(username=USERNAME, password=PASSWORD),
+            json=patient_doc
+        )
+        # ADD THIS LOGGING TO SEE THE ERROR
+        if patch_patient_request.status_code not in [200, 201, 204]:
+            logging.error(f"Failed to update {patient_id}: {patch_patient_request.status_code} - {patch_patient_request.text}")
             
-            except requests.exceptions.RequestException as e:
-                f"Error occurred while saving document '{doc_id}' to '{database}': {str(e)}"
+        return patch_patient_request.status_code
+    return 424
 
-        last_key = f'"{rows[-1]["id"]}"'
 
-        # If less than a full batch was returned, We done!
-        if len(rows) < batch_size:
-            break
+def mark_patients_as_archived(patient_ids):
+    succes_count = 0
+    fail_patches = []
+    for patient_id in tqdm(patient_ids, desc="marking patients"):
+        if patient_id:
+            # ADD 201 HERE - CouchDB returns 201 for successful PUT updates
+            if _patch_request(patient_id) in [200, 201, 204]:
+                succes_count += 1
+            else:
+                fail_patches.append(patient_id)
+    
+    logging.info(f"successful patched patient docs - {succes_count}")
+    if fail_patches:
+        logging.warning(f"failed patched patient docs - {len(fail_patches)}")
+        logging.warning(f"patient ids: {fail_patches}")
 
-if __name__ == "__main__":
-    initialize_setup()
-    active_docs, archive_docs = filter_entries()
-    update_patient_records(archive_docs)
-    save_active_entries(active_docs)
-    house_keeping_please(f"{database}")
-    ensure_database_exists(f"{database}")
-    exodus()
-    house_keeping_please(f"{database}_active")
+
+
+# TEMPORARY MIGRATIONS
+def _helper_pop_revision(doc):
+    doc.pop("_rev", None) 
+    return doc
+
+def _helper_convert_array_to_object(batch):
+    return { "docs": batch }
+
+def _helper_bulk_post(batch, database):
+    put_batch_request = requests.post(
+        url=f"{DB_BASE_URL}{database}/_bulk_docs",
+        auth=HTTPBasicAuth(username=USERNAME, password=PASSWORD),
+        json=_helper_convert_array_to_object(batch=batch)
+    )
+    return put_batch_request.status_code
+
+def run_migration(data, database):
+    tmp_array = []
+    for doc in data:
+        tmp_array.append(_helper_pop_revision(doc))
+    if _helper_bulk_post(batch=tmp_array, database=database) in [200, 201]:
+        logging.info(f"migrated data - {database} - {len(data)}")
+    else:
+        logging.info(f"migration fail - {database} - {len(data)}")
+
+# DELETE DB
+def delete_db(database):
+    if database == "_replicator":
+        url = f"{URL}/{database}"
+    else:
+        url=f"{DB_BASE_URL}{database}"
+    delete_db_request = requests.delete(
+        url=url,
+        auth=HTTPBasicAuth(username=USERNAME, password=PASSWORD)
+    )
+    status_code = delete_db_request.status_code
+    if status_code in [200, 202]:
+        logging.info(f"database deleted sucessfully - {(database or 'primary')} - {status_code}")
+    else:
+        logging.info(f"database delete fail - {(database or 'primary')} - {status_code}")
+    return status_code
+
+
+
+delete_db(database="_replicator")
+
+create_db_if_not_exist()
+
+all_entries = load_entries()
+
+active_entries, archive_entries, patient_ids = filter_entries(all_entries)
+
+if active_entries:
+    run_migration(data=active_entries, database='_active')
+
+if archive_entries:
+    mark_patients_as_archived(patient_ids=patient_ids)
+    run_migration(data=archive_entries, database='_archive')
+
+delete_db(database="")
+
+create_db_if_not_exist()
+
+run_migration(data=active_entries, database="")
+
+delete_db(database="_active")
+
+if DELETE_ARCHIVE:
+    delete_db(database="_archive")

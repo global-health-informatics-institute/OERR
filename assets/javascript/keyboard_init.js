e10ce8b72682d0f85e6353260743392dedbd1703
Isaac Monawe <isaacmonawe6@gmail.com>
2026-05-25 11:46:11 +0200

    enhance: update keyboard layout and button labels for improved usability

--- assets/javascript/keyboard_init.js
+++ assets/javascript/keyboard_init.js
@@ -12,13 +12,15 @@ let keyboard = new Keyboard({
       '1 2 3 4 5 6 7 8 9 0 {bksp}',
       'q w e r t y u i o p',
       '{lock} a s d f g h j k l',
-      'z x c v b n m , . {space}'
+      "z x c v b n m , . '",
+      '{space}'
     ],
     'shift': [
       '1 2 3 4 5 6 7 8 9 0 {bksp}',
       'Q W E R T Y U I O P',
       '{lock} A S D F G H J K L',
-      'Z X C V B N M , . {space}'
+      "Z X C V B N M , . '",
+      '{space}'
     ],
     display: {
       '{bksp}': 'bksp',
